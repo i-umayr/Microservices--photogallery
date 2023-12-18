981325afbcbf00f8bb47f3f1f81103580a677c42
abubakar-javed <abubakarjaved03145005454@gmail.com>
2023-12-18 18:29:18 +0500

    removed some useless folders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { AuthProvider } from "react-auth-kit";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,8 +12,7 @@ root.render(
     cookieDomain={window.location.hostname}
     cookieSecure={false}
   >
-    <Provider store={store}>
       <App />
-    </Provider>
+ 
   </AuthProvider>
 );
