ec746015ffc10559d34d66c14c706549a36c157a
abubakar-javed <abubakarjaved03145005454@gmail.com>
2023-12-25 22:07:30 +0500

    images upload,delete and download functionality working completely. Adjusted cloudinary as well as changed the routes at the backend a bit.

---
+++ frontend/src/components/Images/ExistingImages/ExistingImages.js
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from "react";
+import styles from "./ExistingImages.module.css";
+import axios, { AxiosError } from "axios";
+import { useAuthUser } from "react-auth-kit";
+
+const ExistingImages = ({ images }) => {
+  const [fullScreenImage, setFullScreenImage] = useState(null);
+  const [optionsPosition, setOptionsPosition] = useState({ top: 0, left: 0 });
+  const [showOptions, setShowOptions] = useState(null);
+  const auth = useAuthUser();
+
+  const toggleOptions = (event, image) => {
+    const rect = event.target.getBoundingClientRect();
+    setOptionsPosition({
+      top: rect.bottom + window.scrollY + 5,
+      left: rect.left + window.scrollX,
+    });
+    if (showOptions) {
+      setShowOptions(null);
+    } else {
+      setShowOptions(image);
+    }
+  };
+  const openFullScreen = (image) => {
+    setFullScreenImage(image);
+  };
+
+  const closeFullScreen = () => {
+    setFullScreenImage(null);
+  };
+
+  const handleView = (image) => {
+    setShowOptions(null);
+    openFullScreen(image);
+  };
+  
+  const handleDownload = async (imageLink, imageName) => {
+      setShowOptions(null);
+      try {
+      const response = await fetch(imageLink);
+      const blob = await response.blob();
+
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", imageName);
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    } catch (error) {
+      console.error("Download failed:", error.message);
+    }
+
+};
+
+const handleDelete = async (image) => {
+    setShowOptions(null);
+    try {
+      const userId = auth().userId;
+      const token = auth().token;
+      const config = {
+        headers: {
+          Authorization: `${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      const response = await axios.delete(
+        `http://localhost:4002/images/${userId}/${image._id}`,
+        config
+      );
+      console.log(response)
+    } catch (error) {
+      console.error("Error Deleting image:", error);
+    }
+  };
+  return (
+    <div className={styles.imageGrid}>
+      {images.map((image, index) => (
+        <div className={styles.imageCard} key={index}>
+          <div className={styles.cardHeader}>
+            <p className={styles.imageTitle}>{image.title}</p>
+          </div>
+          <div className={styles.cardBody}>
+            <img
+              src={image.imageLink}
+              className={styles.fullScreenTrigger}
+              onClick={() => openFullScreen(image)}
+            />
+          </div>
+          <div className={styles.cardFooter}>
+            <p className={styles.imageSize}>{Math.ceil(image.size)} KB</p>
+            <div
+              className={styles.optionsButton}
+              onClick={(event) => toggleOptions(event, image)}
+            >
+              ...
+            </div>
+
+            {showOptions && (
+              <div
+                className={styles.options}
+                style={{ top: optionsPosition.top, left: optionsPosition.left }}
+                key={`options_${index}`}
+              >
+                <div
+                  className={styles.option}
+                  onClick={() => handleView(showOptions)}
+                >
+                  View
+                </div>
+                <div
+                  className={styles.option}
+                  onClick={() =>
+                    handleDownload(showOptions.imageLink, showOptions.title)
+                  }
+                >
+                  Download
+                </div>
+                <div
+                  className={styles.option}
+                  onClick={() => handleDelete(showOptions)}
+                >
+                  Delete
+                </div>
+                <div
+                  className={styles.option}
+                  onClick={() => console.log("Rename")}
+                >
+                  Rename
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+      {fullScreenImage && (
+        <>
+          <div className={styles.fullScreenOverlay} onClick={closeFullScreen}>
+            <img
+              src={fullScreenImage.imageLink}
+              alt={fullScreenImage.title}
+              className={styles.fullScreenImage}
+              onClick={(e) => e.stopPropagation()}
+            />
+            <span className={styles.closeButton} onClick={closeFullScreen}>
+              &#10005;
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ExistingImages;
