03e4f1a926285a5fc774895910617b56cdce7967
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-12-30 15:21:30 +0500

    footer changed to modular css

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,58 +1,67 @@
+// Footer.js
+
 import React from "react";
-import './Footer.css';
-import { FaFacebook, FaGithub, FaInstagram, FaYoutube } from 'react-icons/fa';
-import { FaTwitter, FaLinkedin, FaStackOverflow, FaCodepen } from 'react-icons/fa';
+import styles from './Footer.module.css';
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+  FaStackOverflow,
+  FaCodepen
+} from 'react-icons/fa';
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <div className="container">
-                <div className="row">
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.row}>
 
-                    <div className="site-links">
-                        <h3>Explore Gallery</h3>
+          <div className={styles.siteLinks}>
+            <h3>Explore Gallery</h3>
 
-                        <ul className="list-unstyled three-column">
-                            <li>Home</li>
-                            <li>Tools</li>
-                            <li>Places</li>
-                        </ul>
-                        <ul className="list-unstyled three-column">
-                            <li>Services</li>
-                            <li>Source Code</li>
-                            <li>Recommendations</li>
-                        </ul>
-                        <ul className="list-unstyled three-column">
-                            <li>Our Team</li>
-                            <li>About</li>
-                            <li>Code</li>
-                        </ul>
+            <ul className={`${styles.listUnstyled} ${styles.threeColumn}`}>
+              <li>Home</li>
+              <li>Tools</li>
+              <li>Places</li>
+            </ul>
+            <ul className={`${styles.listUnstyled} ${styles.threeColumn}`}>
+              <li>Services</li>
+              <li>Source Code</li>
+              <li>Recommendations</li>
+            </ul>
+            <ul className={`${styles.listUnstyled} ${styles.threeColumn}`}>
+              <li>Our Team</li>
+              <li>About</li>
+              <li>Code</li>
+            </ul>
 
-                        <ul className="list-unstyled socila-list">
-                            <li><FaFacebook /></li>
-                            <li><FaGithub /></li>
-                            <li><FaInstagram /></li>
-                            <li><FaYoutube /></li>
-                            <li><FaTwitter /></li>
-                            <li><FaLinkedin /></li>
-                            <li><FaStackOverflow /></li>
-                            <li><FaCodepen /></li>
-                        </ul>
-                    </div>
+            <ul className={`${styles.listUnstyled} ${styles.socialList}`}>
+              <li><FaFacebook /></li>
+              <li><FaGithub /></li>
+              <li><FaInstagram /></li>
+              <li><FaYoutube /></li>
+              <li><FaTwitter /></li>
+              <li><FaLinkedin /></li>
+              <li><FaStackOverflow /></li>
+              <li><FaCodepen /></li>
+            </ul>
+          </div>
 
-                    <div class="footer-images">
-                        <img className="img-thumbnail" src={process.env.PUBLIC_URL + '/images/placeholder1.png'} alt="Placeholder 1" />
-                    <img className="img-thumbnail" src={process.env.PUBLIC_URL + '/images/placeholder2.png'} alt="Placeholder 2" />
-                    <img className="img-thumbnail" src={process.env.PUBLIC_URL + '/images/placeholder3.png'} alt="Placeholder 3" />
-                    </div>
+          <div className={styles.footerImages}>
+            <img className={styles.imgThumbnail} src={process.env.PUBLIC_URL + '/images/placeholder1.png'} alt="Placeholder 1" />
+            <img className={styles.imgThumbnail} src={process.env.PUBLIC_URL + '/images/placeholder2.png'} alt="Placeholder 2" />
+            <img className={styles.imgThumbnail} src={process.env.PUBLIC_URL + '/images/placeholder3.png'} alt="Placeholder 3" />
+          </div>
 
+        </div>
 
-                </div>
-
-                <div class="copyright text-center">
-                    Copyright &copy; 2023 <span>Made with love 💘 by us</span>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className={styles.copyright}>
+          Copyright &copy; 2023 <span>Made with love 💘 by us</span>
+        </div>
+      </div>
+    </footer>
+  );
 }
